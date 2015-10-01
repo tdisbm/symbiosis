647b6b488d0baae0e7d3b3bcfe0fad8e28436cb7
tdisbm111 <tdisbm1112@gmail.com>
2015-10-01 09:49:49 +0300

    symbiosis update

--- tests/client.js
+++ tests/client.js
@@ -1,3 +1,3 @@
 var io = require("socket.io-client");
 
-io.connect("http://localhost:8000", {query : "type=dev&email=user"});
+io.connect("http://localhost:8000", {query : "type=device&email=user@mail.com"});
