237d5224424ee7adeb0df107d0a7a9a6608f9415
moonwalker <dorin.tufar@freshcells.de>
2016-06-16 20:12:05 +0200

    client hotfix

--- app/tests/client.js
+++ app/tests/client.js
@@ -1,24 +1,47 @@
 var io = require("socket.io-client");
 var argv = require('yargs').argv;
+var Map = require('../../lib/Kernel/Map/Map.js');
 
-var socket = io.connect("http://127.0.0.1:" + argv.port, {
-    query : "email=" + argv.email + "&name1=" + argv.type + "&storage=" + argv.storage
-});
+var map = new Map('../map.json');
+var nodes = map.getElements().nodes;
+var node = null;
 
-socket.on("message", function(data) {
-    console.log(data);
-});
-
-socket.on("error", function(data){
-    console.log(data);
-});
-
-if (argv.type === "device") {
-    console.log("i am device");
-    setInterval(function(){
-        socket.emit("message", {"test" : Math.random()});
-        socket.emit("set-gpio-mask", {"i am data" : Math.random()});
-    }, 2000);
+for (var i = 0, n = nodes.length; i < n; i++) {
+    if (nodes[i].name == argv.node) {
+        node = nodes[i];
+        break;
+    }
 }
+
+if (null === node) {
+    console.info('Node "%s" is not defined on map');
+    process.exit(1);
+}
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || argv.port;
+var host = process.env.OPENSHIFT_NODEJS_IP || argv.host || "127.0.0.1";
+var query = [];
+
+
+for (i = 0, n = node.handshake.parameters.length; i < n; i ++) {
+    if (typeof argv[node.handshake.parameters[i].name] !== 'undefined') {
+        query.push(node.handshake.parameters[i].name + "=" + argv[node.handshake.parameters[i].name]);
+    }
+
+    if (!node.handshake.parameters[i].hasOwnProperty('options')) {
+        continue;
+    }
+
+    if (node.handshake.parameters[i].options.indexOf('identifier') !== -1) {
+        query.push(node.handshake.parameters[i].name + "=" + argv.node)
+    }
+}
+
+console.info("[+] Connect to: \n - Host: http://%s:%s \n - Query: %s", host, port, query.join('&'));
+
+var socket = io.connect("http://" + host + ":" + port, {
+    query : query.join('&')
+});
+
 
 
